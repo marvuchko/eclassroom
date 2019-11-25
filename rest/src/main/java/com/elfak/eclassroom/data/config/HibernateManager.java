98280519a863158f223b8c48c66bdad5a54d2070
Marko Vuckovic <marvuchko@gmail.com>
2019-11-25 11:22:35 +0000

    Update HibernateManager.java

--- rest/src/main/java/com/elfak/eclassroom/data/config/HibernateManager.java
+++ rest/src/main/java/com/elfak/eclassroom/data/config/HibernateManager.java
@@ -62,7 +62,7 @@ public class HibernateManager {
         settings.put(Environment.DIALECT, "org.hibernate.dialect.PostgreSQL10Dialect");
         settings.put(Environment.SHOW_SQL, "true");
         settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
-        settings.put(Environment.HBM2DDL_AUTO, "create-drop");
+        settings.put(Environment.HBM2DDL_AUTO, "update");
         settings.put(Environment.ENABLE_LAZY_LOAD_NO_TRANS, "true");
         return settings;
     }
