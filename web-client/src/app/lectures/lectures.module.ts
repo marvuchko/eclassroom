601805af62b891b1983fa93ccf11f1b0dd116ba8
Stevica Stojković <stevica.j.stojkovic@gmail.com>
2020-01-13 00:24:40 +0100

    Move video threads next to the video player

--- web-client/src/app/lectures/lectures.module.ts
+++ web-client/src/app/lectures/lectures.module.ts
@@ -1,23 +1,30 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatTableModule } from '@angular/material/table';
-import { MatInputModule } from '@angular/material/input';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { NgxSpinnerModule } from 'ngx-spinner';
-import { LecturesListComponent } from './lectures-list/lectures-list.component';
-import { LectureComponent } from './lecture/lecture.component';
-import { AddLectureComponent } from './add-lecture/add-lecture.component';
-import { LecturesRoutingModule } from './lectures-routing.module';
-import { VideoThreadsComponent } from './video-threads/video-threads.component';
-import { VideoPlayerModule } from '../video-player/video-player.module';
-import { AskQuestionComponent } from './ask-question/ask-question.component';
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
+import { MatTableModule } from '@angular/material/table'
+import { MatInputModule } from '@angular/material/input'
+import { MatIconModule } from '@angular/material/icon'
+import { MatButtonModule } from '@angular/material/button'
+import { MatDividerModule } from '@angular/material/divider'
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatSlideToggleModule } from '@angular/material/slide-toggle'
+import { NgxSpinnerModule } from 'ngx-spinner'
+import { LecturesListComponent } from './lectures-list/lectures-list.component'
+import { LectureComponent } from './lecture/lecture.component'
+import { AddLectureComponent } from './add-lecture/add-lecture.component'
+import { LecturesRoutingModule } from './lectures-routing.module'
+import { VideoThreadsComponent } from './video-threads/video-threads.component'
+import { VideoPlayerModule } from '../video-player/video-player.module'
+import { AskQuestionComponent } from './ask-question/ask-question.component'
 
 @NgModule({
-  declarations: [LecturesListComponent, LectureComponent, AddLectureComponent, VideoThreadsComponent, AskQuestionComponent],
+  declarations: [
+    LecturesListComponent,
+    LectureComponent,
+    AddLectureComponent,
+    VideoThreadsComponent,
+    AskQuestionComponent,
+  ],
   imports: [
     CommonModule,
     VideoPlayerModule,
@@ -30,7 +37,8 @@ import { AskQuestionComponent } from './ask-question/ask-question.component';
     MatFormFieldModule,
     MatButtonModule,
     MatIconModule,
-    MatDividerModule
-  ]
+    MatDividerModule,
+    MatSlideToggleModule,
+  ],
 })
-export class LecturesModule { }
+export class LecturesModule {}
