f9e299628f339ec980b291cbc51fff9afdd61edf
Stevica Stojković <stevica.j.stojkovic@gmail.com>
2020-01-12 23:36:32 +0100

    Remove dead code left from component extraction

--- web-client/src/app/lectures/video-threads/video-threads.component.ts
+++ web-client/src/app/lectures/video-threads/video-threads.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { FormControl } from '@angular/forms';
 import { VideoThread } from 'src/app/models/video-thread';
-import { IStartThreadOptions } from 'src/app/models/start-thread-options';
 import { VideoThreadsDataService } from '../video-threads-data.service';
 import { VideoThreadMessage } from 'src/app/models/video-thread-message';
 
@@ -13,62 +11,13 @@ import { VideoThreadMessage } from 'src/app/models/video-thread-message';
 export class VideoThreadsComponent implements OnInit {
 
   @Input()
-  videoId: string;
-
-  @Input()
-  videoTimestamp = 0;
-
-  @Input()
-  videoDuration = 0;
-
-  @Input()
   videoThreads: VideoThread[] = [];
 
-  inEditMode = false;
-
   replyThreadId: string = null;
-
-  titleFormControl: FormControl;
 
   constructor(private threadsService: VideoThreadsDataService) { }
 
   ngOnInit() {
-    this.titleFormControl = new FormControl();
-  }
-
-  startEdit() {
-    this.inEditMode = true;
-  }
-
-  cancel() {
-    this.inEditMode = false;
-    this.titleFormControl.setValue(null);
-  }
-
-  confirm() {
-    const videoId = this.videoId;
-    const title = this.titleFormControl.value;
-    const videoTimestamp = this.videoTimestamp;
-    const authorEmail = prompt('Email');
-    const authorName = prompt('Name');
-    const options: IStartThreadOptions = {
-      videoId,
-      title,
-      videoTimestamp,
-      authorEmail,
-      authorName
-    };
-    this.threadsService.startThread(options).subscribe({
-      complete: () => {
-        this.inEditMode = false;
-        this.titleFormControl.setValue(null);
-      },
-      next: (thread: VideoThread) => this.videoThreads.push(thread),
-      error: (error: any) => {
-        console.log(error);
-        alert('Failed to start new thread');
-      }
-    });
   }
 
   startReply(videoThread: VideoThread) {
